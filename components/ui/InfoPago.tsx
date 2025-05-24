51bf21b1388b0d8af2242f82dc2e8c77705213b8
juancho391 <juanchischis2010@hotmail.com>
2025-05-24 13:06:26 -0500

    redireccion a la pagina inicio con el boton desde la pagina de pago aprobado o rechazado

--- components/ui/InfoPago.tsx
+++ components/ui/InfoPago.tsx
@@ -1,18 +1,20 @@
 import { MdOutlineCheckCircle } from "react-icons/md";
 import { GoXCircle } from "react-icons/go";
+import { useRouter } from "next/navigation";
 export interface InfoSolicitud {
   text: string;
   aprobado: boolean;
 }
 
 export default function InfoPago(props: InfoSolicitud) {
+  const router = useRouter();
   const mensaje = props.aprobado
     ? "Tu pago se ha realizado con exito los numeros de tu boleta se enviaran a tu correo electronico"
     : "Algo salio mal no pudimos procesar tu pago intentalo nuevamente";
   return (
     <div className="flex items-center flex-col p-3 space-y-7 sm:space-y-0 text-center">
       {/* <Header /> */}
-      <div className="flex flex-col items-center border-2 border-brandYellow rounded-md w-full p-3 gap-5">
+      <div className="flex flex-col items-center  rounded-md w-full p-3 gap-5">
         {props.aprobado ? (
           <div className="text-9xl text-green-500">
             <MdOutlineCheckCircle />
@@ -30,6 +32,9 @@ export default function InfoPago(props: InfoSolicitud) {
           className={`w-30 h-10 border-1 m-5 border-white rounded-sm cursor-pointer ${
             props.aprobado ? "bg-green-700" : "bg-brandRed"
           }`}
+          onClick={() => {
+            router.push("/");
+          }}
         >
           Volver al inicio
         </button>
